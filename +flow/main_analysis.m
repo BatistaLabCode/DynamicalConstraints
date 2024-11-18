a3844eebb1f67fb1eaa70261beea101d12c7709e
Erinn Grigsby <erinn.grigsby@gmail.com>
2024-11-18 12:19:39 -0500

    Update main_analysis.m
    Update header file and clean-up a few coding inconsistency for naming conventions.

--- +flow/main_analysis.m
+++ +flow/main_analysis.m
@@ -1,18 +1,21 @@
-% Main analysis function for flow analysis
+% flow.main_analysis Main analysis function for flow analysis
+%
+% Usage:
+%   flow.main_analysis(base_dir, intuitive_dir, rotated_dir)
 %
 % This function serves as a testbed for the neural flow analysis used to
 % characterize similarity in dynamics between conditions.
 %
 % Inputs:
-%   base_dir        Paths for the main data folder
-%   intuitive_dir   Paths for the main data folder
-%   rotated_dir     Paths for the main data folder
+%   base_dir        Path for the main data folder
+%   intuitive_dir   Path for the movement intuitive (MovInt) data folder
+%   rotated_dir     Path for the separation maximizing (SepMax) data folder
 %
 % Optional Inputs:
-%   d_grid              The size of the each workspace voxel grid.
-%   min_pts_per_voxel   Minmum number of points contained in a valid voxel
-%   data_save_loc       Location to save flow field session data.
-%   saveData            Determines whether save a FlowResult structure in
+%   d_grid             The size of the each workspace voxel grid.
+%   min_pts_per_voxel  Minimum number of voxels to include for calculations
+%   data_save_loc      Location to save flow field session data.
+%   saveData           Determines whether save a FlowResult structure in
 %                           data_save_loc folder.
 % 
 % Outputs:
@@ -20,17 +23,16 @@
 %                           used in the main paper figures and analysis for
 %                           a given session.
 % FlowResult structure is:
-%   FF_int:     Flow Field (FF) for the movement intuitive (MovInt) trials
-%   FF_int_rev: FF for the time reversed MovInt trials  
-%   FF_pred:    FF for MovInt trials projected into the separation
-%                   maximizing (SepMax) workspace
-%   FF_rot:     FF for the SepMax trials
-%   FF_pred_Int FF for SepMax trials projected into the MovInt workspace
-%   A_int_rot:  Comparison of FF_int and FF_rot
-%   A_pred_rot: Comparison of FF_pred and FF_rot
-%   stat:       Stats comparing error distribution for A_int_rot and A_pred_rot
+%   FF_int:      Flow Field (FF) for the MovInt trials
+%   FF_int_rev:  FF for the time reversed MovInt trials  
+%   FF_pred:     FF for MovInt trials projected into the SepMax workspace
+%   FF_rot:      FF for the SepMax trials
+%   FF_pred_Int: FF for SepMax trials projected into the MovInt workspace
+%   A_int_rot:   Comparison of FF_int and FF_rot
+%   A_pred_rot:  Comparison of FF_pred and FF_rot
+%   stat:        Stats comparing error distribution for A_int_rot and A_pred_rot
 %
-% Copyright (C) by Alan Degenhart and Erinn Grigsby
+% Copyright (C) by Erinn Grigsby and Alan Degenhart 
 % Emails: erinn.grigsby@gmail.com or alan.degenhart@gmail.com
 
 function [FlowResults] = main_analysis(base_dir, intuitive_dir, rotated_dir, varargin)
@@ -38,14 +40,14 @@ function [FlowResults] = main_analysis(base_dir, intuitive_dir, rotated_dir, var
 % Parse optional arguments.
 d_grid = 20;           % Allow the grid to be pre-specified
 min_pts_per_voxel = 2; % Minmum number of points contained in a valid voxel
+centerPos = [0 0 0];   % Defined center of the workspace
 % Define locations to save results structure
 data_save_loc = 'F:\Erinn\testSave\FlowComparison\mat';
 saveData = 1;
 
 assignopts (who, varargin);
 
-% Define dataset locations.  Eventually we will want to replace this with
-% some sort of loop over valid datasets
+% Define dataset locations.
 D.base = base_dir;
 D.two_target_intuitive = intuitive_dir;
 D.two_target_rotated = rotated_dir;
@@ -54,18 +56,27 @@ D.two_target_rotated = rotated_dir;
 TD_int = TrajectoryData().load(D.two_target_intuitive);
 TD_rot = TrajectoryData().load(D.two_target_rotated);
 
-if ~ismember(mean(unique([TD_rot.startPos]')),[0 0 0])
-TD_int = TD_int.normalize(mean(unique([TD_rot.startPos]','rows')));
-TD_rot = TD_rot.normalize(mean(unique([TD_rot.startPos]','rows')));
+if ~ismember(mean(unique([TD_rot.startPos]')),centerPos)
+    TD_int = TD_int.normalize(mean(unique([TD_rot.startPos]','rows')));
+    TD_rot = TD_rot.normalize(mean(unique([TD_rot.startPos]','rows')));
+else
+    TD_int = TD_int.normalize(centerPos);
+    TD_rot = TD_rot.normalize(centerPos);
 end
 
 % Get subject, dataset, and decoder info and load decoder
-if ismember(TD_int(1).subject,'Monkey E')
+if ismember(TD_int(1).subject,{'Monkey E'})
     subject = 'monkeyE';
-elseif ismember(TD_int(1).subject,'Monkey D')
+    TD_int(1).subject = 'Monkey E';
+    TD_rot(1).subject = 'Monkey E';
+elseif ismember(TD_int(1).subject,{'Monkey D'})
     subject = 'monkeyD';
-elseif ismember(TD_int(1).subject,'Monkey Q')
+    TD_int(1).subject = 'Monkey D';
+    TD_rot(1).subject = 'Monkey D';
+elseif ismember(TD_int(1).subject,{'Monkey Q'})
     subject = 'monkeyQ';
+    TD_int(1).subject = 'Monkey Q';
+    TD_rot(1).subject = 'Monkey Q';
 else
     subject = TD_int(1).subject;
 end
@@ -98,14 +109,15 @@ cond_str = 'SepMax_Predicted';
 
 % Run analysis for rotated two-target trials
 cond_str = 'Separation Max';
-[FF_rot] = flow.calc_flow_field(TD_rot, d_grid, cond_str);
+[FF_rot] = flow.calc_flow_field(TD_rot, d_grid, cond_str, ...
+    'min_pts_per_voxel', min_pts_per_voxel);
 
 % Load the intuitive mapping
 int_map_path = fullfile(D.base, D.intMov_mapping);
 M = load(int_map_path);
 
 % Apply intuitive mapping to rot trajectories
-TD_int_pred = util.predictDecodeState_GPFA(TD_int, M.bci_params, ...
+TD_int_pred = util.predictDecodeState_GPFA(TD_rot, M.bci_params, ...
     'spikeCountSrc', 'decodeSpikeCounts');
 cond_str = 'IntMov_Predicted';
 [FF_pred_Int] = flow.calc_flow_field(TD_int_pred, d_grid, cond_str, ...
@@ -113,15 +125,15 @@ cond_str = 'IntMov_Predicted';
 
 % Compare flow fields
 [A_int_rot] = flow.compare_flow_fields(FF_int, FF_rot,'saveRaw',0,...
-    'subject', subject, 'dataset', dataset, 'cond_str', 'IntVsRot');
+    'subject', subject, 'dataset', dataset, 'cond_str', 'IntMovVsSepMax');
 [A_pred_rot] = flow.compare_flow_fields(FF_pred, FF_rot,'saveRaw',0,...
-    'subject', subject, 'dataset', dataset, 'cond_str', 'PredVsRot');
+    'subject', subject, 'dataset', dataset, 'cond_str', 'PredVsSepMax');
 
 % Summarize session (statistical tests)
 [stats] = flow.calc_session_stats(A_int_rot, A_pred_rot);
 
 % Add data to results structure
-FlowResults.subject = subject;
+FlowResults.subject = TD_int(1).subject;
 FlowResults.dataset = dataset;
 FlowResults.FF_int = FF_int;
 FlowResults.FF_int_rev = FF_int_rev;
@@ -134,9 +146,9 @@ FlowResults.stats = stats;
 
 % Save results
 intPos = regexp(intuitive_dir,'_');
-intPos = [intPos(1)+1:intPos(2)-1];
+intPos = intPos(1)+1:intPos(2)-1;
 rotPos = regexp(rotated_dir,'_');
-rotPos = [rotPos(1)+1:rotPos(2)-1];
+rotPos = rotPos(1)+1:rotPos(2)-1;
 if saveData
     mkdir(data_save_loc)
     save_filename = sprintf('%s%s_int%s_rot%s_FlowResults.mat', subject,...
